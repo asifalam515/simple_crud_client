5734373ed831a828eaeb9e5946d1c4c63681612b
asifalam515 <asibulalam515@gmail.com>
2024-09-24 15:32:37 +0600

    create read and delete applided

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {
+      name,
+      email,
+    };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("users added successfully");
+          form.reset();
+        }
+      });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" name="submit" value="Add User" />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+  const loadedUser = useLoaderData();
+  const [users, setUsers] = useState(loadedUser);
+  const handleDeleteUser = (_id) => {
+    console.log("delete", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("delete successfully id:", _id);
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
+  };
+  return (
+    <div>
+      {<h2>{users.length}</h2>}
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} :{user.email}{" "}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
